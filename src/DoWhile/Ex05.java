d888140d2489ad95b89c79d2e7c5d7167dc3a057
Carla <carla.jemaitis@gmail.com>
2024-10-31 11:12:38 -0300

    Refatorei o Ex05.

--- src/DoWhile/Ex05.java
+++ src/DoWhile/Ex05.java
@@ -7,11 +7,9 @@ public class Ex05
    public static void main(String[] args)
    {
 
-
    Scanner leia = new Scanner(System.in);
    int num, soma;
    soma = 0;
-
 
    do
    {
@@ -23,7 +21,6 @@ public class Ex05
            }
    }
    while(num != 0);
-
 
    System.out.println("A soma dos números é: " + soma);
    leia.close();
